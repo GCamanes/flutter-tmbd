1d8b1c3eead1fc86ef0fa4f7a3a1ac66ce1dc737
gcamanes <guillaume.camanes.prestataire@wiztivi.com>
2023-01-12 22:25:37 +0100

    added FilmEntity and FilmModel

---
+++ lib/entities/film.entity.dart
@@ -0,0 +1,35 @@
+class FilmEntity {
+  const FilmEntity({
+    required this.adult,
+    this.background,
+    required this.budget,
+    required this.id,
+    required this.language,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    this.poster,
+    this.releaseDate,
+    required this.revenue,
+    this.tagLine,
+    required this.title,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  final bool adult;
+  final String? background;
+  final int budget;
+  final int id;
+  final String language;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String? poster;
+  final String? releaseDate;
+  final int revenue;
+  final String? tagLine;
+  final String title;
+  final double voteAverage;
+  final int voteCount;
+}
---
+++ lib/models/film.model.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_tmbd/entities/film.entity.dart';
+
+class FilmModel extends FilmEntity {
+  const FilmModel({
+    required bool adult,
+    String? background,
+    required int budget,
+    required int id,
+    required String language,
+    required String originalTitle,
+    required String overview,
+    required double popularity,
+    final String? poster,
+    String? releaseDate,
+    required int revenue,
+    final String? tagLine,
+    required String title,
+    required double voteAverage,
+    required int voteCount,
+  }) : super(
+          adult: adult,
+          background: background,
+          budget: budget,
+          id: id,
+          language: language,
+          originalTitle: originalTitle,
+          overview: overview,
+          popularity: popularity,
+          poster: poster,
+          releaseDate: releaseDate,
+          revenue: revenue,
+          tagLine: tagLine,
+          title: title,
+          voteAverage: voteAverage,
+          voteCount: voteCount,
+        );
+
+  factory FilmModel.fromJson(Map<String, dynamic> json) {
+    return FilmModel(
+      adult: json['adult'] ?? false,
+      background: json['backdrop_path'],
+      budget: json['budget'] ?? 0,
+      id: json['id'] ?? 0,
+      language: json['original_language'] ?? '',
+      originalTitle: json['original_title'] ?? '',
+      overview: json['overview'] ?? '',
+      popularity: json['popularity'] ?? 0.0,
+      poster: json['poster_path'],
+      releaseDate: json['release_date'],
+      revenue: json['runtime'] ?? 0,
+      tagLine: json['tagline'] ?? '',
+      title: json['title'] ?? '',
+      voteAverage: json['vote_average'] ?? 0.0,
+      voteCount: json['vote_count'] ?? 0,
+    );
+  }
+}
